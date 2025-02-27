6615eeeb8363e7066d38749c85127ded5b665adf
JeffRisberg <j.s.risberg@gmail.com>
2025-02-27 15:25:20 -0800

    added example of component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,16 @@
+import React from "react";
+import Marker from "../components/marker";
+import Button from "../components/button";
 import styles from "./pages.module.css";
 
 const DashboardPage = () => (
-    <div className={styles.page}>
-        <h1>Dashboard Page</h1>
-    </div>
+   <div className={styles.page}>
+      <h1>Dashboard Page</h1>
+
+      <Marker/>
+      <Button/>
+      <Marker/>
+   </div>
 );
 
 export default DashboardPage;
