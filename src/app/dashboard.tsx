e2e5aff5665d053cf141eb0efd90bb6b796c9f84
JeffRisberg <j.s.risberg@gmail.com>
2025-02-27 11:40:21 -0800

    added dashboard page.

---
+++ src/app/dashboard.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const DashboardPage = () => (
+    <div>
+        <Link href="/dashboard">
+            <a>Dashboard Page</a>
+        </Link>
+    </div>
+);
+
+export default DashboardPage;
